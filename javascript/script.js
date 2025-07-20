54de6f5788bd6ab6562eba71be1aee0677ff2d64
Caden Sandusky <168572526+Narwhal20@users.noreply.github.com>
2025-07-20 17:43:12 -0400

    created score variables, minor syntax fix

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,12 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3) + 1;  // generates a random whole number between 1-3
     console.log("Comp number: " + randomNumber); // This will output 1, 2, or 3
-    return randomNumber 
+    return randomNumber; 
 }
 function getHumanChoice(userInput = prompt("What do you choose? rock, paper, or scissors? type first letter only. ")){ // prompts user for r,p, or s.
-    console.log("this is your choice... " +  userInput) //outputs users choice
+    console.log("this is your choice... " +  userInput); //outputs users choice
 
 }
