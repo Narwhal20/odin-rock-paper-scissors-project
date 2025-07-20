dfb162e4d03d3ae4243ddd946da24e14fc032f60
Caden Sandusky <168572526+Narwhal20@users.noreply.github.com>
2025-07-20 17:41:43 -0400

    getComputerChoice function created

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,5 @@
-console.log("Hello World!");
+function getComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * 3) + 1;  // generates a random whole number between 1-3
+    console.log("Comp number: " + randomNumber); // This will output 1, 2, or 3
+    return randomNumber 
+}
