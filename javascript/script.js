d6a06d216b0dfa3d467e3e1402a8a882d68a57e9
Caden Sandusky <168572526+Narwhal20@users.noreply.github.com>
2025-07-20 17:42:20 -0400

    getHumanChoice function created

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,7 @@ function getComputerChoice(){
     console.log("Comp number: " + randomNumber); // This will output 1, 2, or 3
     return randomNumber 
 }
+function getHumanChoice(userInput = prompt("What do you choose? rock, paper, or scissors? type first letter only. ")){ // prompts user for r,p, or s.
+    console.log("this is your choice... " +  userInput) //outputs users choice
+
+}
